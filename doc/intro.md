24acad672244d895d03a8da608106c3605a33d7f
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-10-22 15:34:02 +0100

    Moved code from photon

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to photon-db
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tranchis/photon-db "0.9.1"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
