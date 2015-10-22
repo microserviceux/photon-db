66c2049a154dd03524d35ce30ba6d988ea6feb5a
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-10-22 15:43:21 +0100

    Parallelise computation, fix typos

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tranchis/photon-db "0.9.1"
+(defproject tranchis/photon-db "0.9.3"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
